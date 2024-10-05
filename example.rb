a650295b170e19f3f76c581c8ae0dbc30d7e9174
milkeclair <milkeclair.black@gmail.com>
2024-10-05 20:46:18 +0900

    feat: 例にディレクトリのパターンを追加

--- example.rb
+++ example.rb
@@ -3,4 +3,8 @@
 require_relative "lib/jp_quest"
 
 performer = JpQuest::Performer.new(output_logs: false)
-performer.perform(".snbt")
+# dir_path = "quests"
+file_path = "hoge.snbt"
+
+performer.perform(file_path)
+# performer.perform_directly(dir_path: dir_path)
