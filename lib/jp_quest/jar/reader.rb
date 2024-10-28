67cb35e23b58fe70569b86d3d383ed00a7cfe2e1
milkeclair <milkeclair.black@gmail.com>
2024-10-28 12:51:00 +0900

    refactor: よりわかりやすいようにcountry_nameに変更

--- lib/jp_quest/jar/reader.rb
+++ lib/jp_quest/jar/reader.rb
@@ -4,9 +4,9 @@ require "countries"
 module JpQuest
   module Jar
     class Reader
-      def initialize(file_path, country)
+      def initialize(file_path, country_name)
         @file_path = file_path
-        @country = country
+        @country_name = country_name
       end
 
       def read
@@ -16,27 +16,27 @@ module JpQuest
         end
       end
 
-      def find_lang_json(zip_file, country = @country)
+      def find_lang_json(zip_file, country_name = @country_name)
         zip_file.each do |entry|
           # langフォルダ以外は探索不要
           next unless entry.name.include?("lang")
 
           file_name = extract_file_name(entry.name)
-          return entry if locale_file_format?(file_name, country)
+          return entry if locale_file_format?(file_name, country_name)
         end
-      end
-
-      def to_country_code(country)
-        ISO3166::Country.find_country_by_any_name(country).alpha2.downcase
-      end
-
-      def locale_file_format?(file_name, country)
-        country_code = to_country_code(country)
-        file_name.include?("_#{country_code}.json")
       end
 
       def extract_file_name(file)
         file.split("/").last
+      end
+
+      def locale_file_format?(file_name, country_name)
+        country_code = to_country_code(country_name)
+        file_name.include?("_#{country_code}.json")
+      end
+
+      def to_country_code(country_name)
+        ISO3166::Country.find_country_by_any_name(country_name).alpha2.downcase
       end
     end
   end
