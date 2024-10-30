a18222e9c86d5de4bd51ba491d1a3993a3f22742
milkeclair <milkeclair.black@gmail.com>
2024-10-31 01:16:18 +0900

    feat: writerに動作確認用の処理を追加

--- lib/jp_quest/jar/writer.rb
+++ lib/jp_quest/jar/writer.rb
@@ -1,8 +1,14 @@
 module JpQuest
   module JAR
     class Writer
-      def initialize(file_path)
-        @file_path = file_path
+      def initialize
+        @output_path_base = "output/mods/jpQuest-#{Time.now.strftime("%Y-%m-%d")}"
+        @output_path = nil
+      end
+
+      def make_resource_pack(results)
+        @output_path = "#{@output_path_base}/#{results.file_name}"
+        puts @output_path
       end
     end
   end
