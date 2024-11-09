1eb50d14cce9ef3474a3d42fbc47e2eab2a6cf2b
milkeclair <milkeclair.black@gmail.com>
2024-11-09 16:57:56 +0900

    feat: ロジックをメソッドに隠蔽、ファイルの作成処理を追加

--- lib/jp_quest/jar/writer.rb
+++ lib/jp_quest/jar/writer.rb
@@ -7,9 +7,23 @@ module JpQuest
       end
 
       def make_resource_pack(results)
-        results.file_name = results.file_name.name.gsub("en_us", results.locale_code)
-        @output_path = "#{@output_path_base}/#{results.file_name}"
-        puts @output_path
+        results.file_name = replace_default_locale_code(results.file_name.name, results.locale_code)
+        @output_path = merge_base_path(results.file_name)
+        make_file(@output_path)
+      end
+
+      private
+
+      def replace_default_locale_code(file_name, locale_code)
+        file_name.gsub("en_us", locale_code)
+      end
+
+      def merge_base_path(file_name)
+        "#{@output_path_base}/#{file_name}"
+      end
+
+      def make_file(path)
+        FileUtils.mkdir_p(path) unless File.exist?(File.expand_path(path))
       end
     end
   end
